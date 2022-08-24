27f70ef93049acdebe7b867d879eba4b2af55a43
alex122is <44220856+alex122is@users.noreply.github.com>
2022-08-24 13:01:30 -0500

    changing data

--- contact_me.php
+++ contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['Message'];
 
 	
 // Create the email and send the message
-$to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'a.ibarra1712@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\n Phone: $phone \n\n City: $city \n\nMessage:\n$message";
 $headers = "From: Info@commercial-gas-engineer.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
